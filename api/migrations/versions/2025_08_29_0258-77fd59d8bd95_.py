7c60f6a854c0707ab747e8355c9c940de3617bf6
GareArc <chen4851@purdue.edu>
2025-08-29 02:58:53 -0700

    merge heads

---
+++ api/migrations/versions/2025_08_29_0258-77fd59d8bd95_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 77fd59d8bd95
+Revises: 8d289573e1da, b45e25c2d166
+Create Date: 2025-08-29 02:58:49.644206
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '77fd59d8bd95'
+down_revision = ('8d289573e1da', 'b45e25c2d166')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
